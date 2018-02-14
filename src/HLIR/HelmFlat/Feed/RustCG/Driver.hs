9f27c0ce0ea99f80d4506cf6fe434956b2604a04
Colbyn Wadman <hello@colbyn.com>
2018-02-14 15:50:55 -0700

    Added HelmFlat to RustCG AST conversions.

---
+++ src/HLIR/HelmFlat/Feed/RustCG/Driver.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module HLIR.HelmFlat.Feed.RustCG.Driver (
+    toRustCG
+  , toRustCG'
+) where
+
+
+-- *
+import Core
+import Core.Control.Flow ((|>), (<|))
+import Core.List.Util    (flatten, singleton)
+import Data.Monoid ((<>))
+import Prelude
+    ( return
+    , String
+    , IO
+    , show
+    , error
+    , (<$>)
+    , (>>=)
+    , (>>)
+    , fromIntegral
+    )
+
+import qualified Prelude    as Pre
+import qualified Core.Utils as Core
+
+import qualified Control.Monad              as M
+import qualified Control.Monad.State        as M
+import qualified Control.Monad.Except       as M
+import qualified Control.Monad.RWS          as M
+import qualified Control.Monad.Identity     as M
+import qualified Control.Monad.Reader       as M
+import qualified Control.Monad.Writer       as M
+import qualified Control.Monad.Trans        as M
+
+import qualified Data.List                    as List
+import qualified Data.Text                    as Text
+import qualified Data.Text.IO                 as TIO
+import qualified Data.Map                     as Map
+import qualified Data.Set                     as Set
+import qualified Data.Foldable                as Fold
+import qualified Data.Monoid                  as Monoid
+import qualified Data.Maybe                   as Maybe
+import qualified Data.Either                  as Either
+import qualified Data.Char                    as Char
+import qualified Data.Word                    as Word
+import qualified Data.STRef                   as ST
+import qualified Data.Bits                    as Bit
+import qualified Data.Fixed                   as Fixed
+import qualified Data.Vector.Unboxed          as V
+import qualified Data.Vector.Unboxed.Mutable  as MV
+import qualified Data.Vector.Generic          as VG
+import qualified Data.IORef                   as IORef
+import qualified Data.ByteString              as BS
+import qualified Data.Functor                 as Fun
+
+-- + Recursion Schemes & Related
+import qualified Data.Functor.Foldable       as F
+import qualified Data.Generics.Uniplate.Data as Uni
+
+-- + OS APIS & Related
+import qualified System.IO as SIO
+
+-- + Dev & Debugging
+import qualified Text.Show.Prettyprint as PP
+
+-- + HelmFlat AST Interface
+import qualified HLIR.HelmFlat.Data.Interface as HelmFlat
+-- + RustCG AST Interface
+import qualified GCIR.RustCG.Data.Interface   as RustCG
+
+-- + Local
+import qualified HLIR.HelmFlat.Feed.RustCG.Syntax as Syntax
+-- *
+
+
+toRustCG :: IO (Either Text HelmFlat.Program) -> IO (Either Text RustCG.Program)
+toRustCG upstream = do
+    result <- upstream
+    
+    case result of
+        Left err -> return $ Left err
+        Right payload ->
+            return
+                $ Right
+                $ toRustCG' payload
+
+
+
+
+toRustCG' :: HelmFlat.Program -> RustCG.Program
+toRustCG' payload =
+    let
+        fns = HelmFlat.getFunctions payload
+            |> map Syntax.dropFunction
+        uns = HelmFlat.getUnions payload
+            |> map Syntax.dropUnion
+    in
+        RustCG.Program
+            { RustCG.enums = uns
+            , RustCG.functions =fns
+            }
+
+
+
+
+
+
+
+
+
